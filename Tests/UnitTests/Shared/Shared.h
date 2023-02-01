40e9efbb79a1ba37ccd914c0358e6b803948ddae
Gary Hsu <bghgary@users.noreply.github.com>
2023-02-01 14:12:42 -0800

    Add macOS unit tests (#7)

--- Tests/UnitTests/Shared/Shared.h
+++ Tests/UnitTests/Shared/Shared.h
@@ -2,4 +2,6 @@
 
 #include <Babylon/Polyfills/Console.h>
 
+const char* EnumToString(Babylon::Polyfills::Console::LogLevel logLevel);
+
 int RunTests(Babylon::Polyfills::Console::CallbackT consoleCallback);
