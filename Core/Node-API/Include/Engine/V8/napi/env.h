5b1d5d415481b76bf342868a2889232c99126265
Gary Hsu <bghgary@users.noreply.github.com>
2023-06-09 10:13:41 -0700

    Minor fixes

--- Core/Node-API/Include/Engine/V8/napi/env.h
+++ Core/Node-API/Include/Engine/V8/napi/env.h
@@ -1,11 +1,15 @@
 #pragma once
 
-#include <napi/env_shared.h>
+#include <napi/napi.h>
 #include <v8.h>
 
 namespace Napi
 {
   Napi::Env Attach(v8::Local<v8::Context>);
+
+  void Detach(Napi::Env);
+
+  Napi::Value Eval(Napi::Env env, const char* source, const char* sourceUrl);
 
   v8::Local<v8::Context> GetContext(Napi::Env);
 }
