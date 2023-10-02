e22ac5723491b095975a67474e80893cd60fe50b
Andy Fillebrown <afillebrown@microsoft.com>
2023-10-02 11:55:30 -0400

    Update UrlLib dependency and add symlink unit tests

--- Tests/UnitTests/Scripts/tests.js
+++ Tests/UnitTests/Scripts/tests.js
@@ -165,6 +165,20 @@ describe("XMLHTTPRequest", function () {
         expect(xhr).to.have.property('readyState', 4);
         expect(xhr).to.have.property('status', 200);
     });
+
+    if (hostPlatform === "macOS" || hostPlatform === "Unix" || hostPlatform === "Win32") {
+        it("should load URL pointing to symlink", async function () {
+            this.timeout(1000);
+            const xhr = await createRequest("GET", "app:///Scripts/symlink_1.js");
+            expect(xhr).to.have.property('responseText', 'var symlink_target_js = true;');
+        });
+
+        it("should load URL pointing to symlink that points to a symlink", async function () {
+            this.timeout(1000);
+            const xhr = await createRequest("GET", "app:///Scripts/symlink_2.js");
+            expect(xhr).to.have.property('responseText', 'var symlink_target_js = true;');
+        });
+    }
 });
 
 describe("setTimeout", function () {
