b0a0a829cde2d9e6995532954d79b65ec96a490e
Carolina Herbster <carolhmj@gmail.com>
2023-10-20 11:39:14 -0300

    do redirection trick in Android

--- Tests/UnitTests/Android/app/src/main/cpp/JNI.cpp
+++ Tests/UnitTests/Android/app/src/main/cpp/JNI.cpp
@@ -4,6 +4,56 @@
 #include <AndroidExtensions/JavaWrappers.h>
 
 #include <Shared/Shared.h>
+#include <unistd.h>
+
+namespace
+{
+    int pfd[2];
+    int fd_saved[2];
+}
+
+void* thread_func(void*)
+{
+    ssize_t rdsz;
+    char buf[128];
+    while ((rdsz = read(pfd[0], buf, sizeof buf - 1)) > 0)
+    {
+        if (buf[rdsz - 1] == '\n') --rdsz;
+        buf[rdsz] = 0;
+        __android_log_write(ANDROID_LOG_DEBUG, "UnitTests", buf);
+    }
+    __android_log_write(ANDROID_LOG_DEBUG, "UnitTests", "Logger shutdown");
+    return 0;
+}
+
+void start_logger()
+{
+    pthread_t thr;
+
+    // make stdout line-buffered and stderr unbuffered
+    fd_saved[0] = setvbuf(stdout, 0, _IOLBF, 0);
+    fd_saved[1] = setvbuf(stderr, 0, _IONBF, 0);
+
+    // create the pipe and redirect stdout and stderr
+    pipe(pfd);
+    dup2(pfd[1], 1);
+    dup2(pfd[1], 2);
+
+    // spawn the logging thread
+    if (pthread_create(&thr, 0, thread_func, 0) == -1)
+    {
+        return;
+    }
+    pthread_detach(thr);
+}
+
+void stop_logger()
+{
+    close(pfd[1]);
+    close(pfd[0]);
+    setvbuf(stdout, NULL, fd_saved[0], 0);
+    setvbuf(stderr, NULL, fd_saved[1], 0);
+}
 
 extern "C" JNIEXPORT jint JNICALL
 Java_com_jsruntimehost_unittests_Native_javaScriptTests(JNIEnv* env, jclass clazz, jobject context) {
@@ -15,25 +65,15 @@ Java_com_jsruntimehost_unittests_Native_javaScriptTests(JNIEnv* env, jclass claz
 
     jclass webSocketClass{env->FindClass("com/jsruntimehost/unittests/WebSocket")};
     java::websocket::WebSocketClient::InitializeJavaWebSocketClass(webSocketClass, env);
+
+    start_logger();
+
     android::global::Initialize(javaVM, context);
 
-    auto consoleCallback = [](const char* message, Babylon::Polyfills::Console::LogLevel level)
-    {
-        switch (level)
-        {
-        case Babylon::Polyfills::Console::LogLevel::Log:
-            __android_log_write(ANDROID_LOG_INFO, "JsRuntimeHost", message);
-            break;
-        case Babylon::Polyfills::Console::LogLevel::Warn:
-            __android_log_write(ANDROID_LOG_WARN, "JsRuntimeHost", message);
-            break;
-        case Babylon::Polyfills::Console::LogLevel::Error:
-            __android_log_write(ANDROID_LOG_ERROR, "JsRuntimeHost", message);
-            break;
-        }
-    };
+    auto testResult = RunTests();
 
-    auto testResult = RunTests(consoleCallback);
+    stop_logger();
+
     java::websocket::WebSocketClient::DestructJavaWebSocketClass(env);
     return testResult;
 }
