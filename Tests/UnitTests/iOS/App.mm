ef3bb85d2e8e1ec314aaa2ff0df7fba360acd1b1
Gary Hsu <bghgary@users.noreply.github.com>
2024-03-21 15:35:28 -0700

    Minor fixes (#85)
    - Remove UWP tests to avoid confusion since they don't work
    - Clean up the shared test code a bit
    - Add SetConsoleOutputCP to Win32 test to get Unicode console output

--- Tests/UnitTests/iOS/App.mm
+++ Tests/UnitTests/iOS/App.mm
@@ -1,11 +1,13 @@
 #include "../Shared/Shared.h"
+#include <iostream>
 
 int main()
 {
     int exitCode = RunTests();
 
     // CI will pick up the exit code from stderr when running in the iOS Simulator.
-    fprintf(stderr, "%i\n", exitCode);
+    std::cerr << exitCode << std::endl;
+    std::cerr.flush();
 
     return exitCode;
 }
